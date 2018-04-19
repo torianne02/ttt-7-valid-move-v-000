5397b4cb448ecbad3c77edef7874c655c224583a
Victoria Fluharty <victoria.fluharty@gmail.com>
2018-04-19 18:36:21 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
  if index.between?(0, 8) && (board[index] == " " || board[index] == "" || board[index] == nil)
+   return true
  end
 end
 
